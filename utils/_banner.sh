d0ea2ae2a133f9c5c75b788a887c9d1d9f2882a6
juniocristian <juniocristianmg@gmail.com>
2023-10-14 12:55:07 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -1,9 +1,38 @@
- ________      ________      ________   
-|\_____  \    |\   __  \    |\   __  \  
- \|___/  /|   \ \  \|\  \   \ \  \|\  \ 
-     /  / /    \ \   __  \   \ \   ____\
-    /  /_/__    \ \  \ \  \   \ \  \___|
-   |\________\   \ \__\ \__\   \ \__\   
-    \|_______|    \|__|\|__|    \|__|   
-                                        
-                                        
+#!/bin/bash
+#
+# Print banner art.
+
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+  printf "${GREEN}";
+                                    
+  printf "${NC}";
+
+  printf "\n"
+
+printf "${GREEN}";  
+printf "Instalação do script Whaticket Zap\n";
+
+printf "${NC}";
+  
+  
+
+  printf "\n"
+}
